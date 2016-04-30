937acbde51793c1fe13899ac4e1fc10b388927b2
ericwlange <eric@flicket.tv>
2016-04-30 10:11:30 +0530

    Removed deprecated scripts (moved to hemroid) Assume hemroid in path

--- scripts/build_aar.sh
+++
@@ -1,9 +0,0 @@
-#set JNI_LIBS
-#set AAR_INSTALL
-
-set -e
-
-cd ${JNI_LIBS}/../../../../
-./gradlew assembleRelease
-mkdir -p ${AAR_INSTALL}
-install -C AndroidJSCore/build/outputs/aar/*-release.aar ${AAR_INSTALL} 
